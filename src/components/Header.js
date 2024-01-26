4394e1490e6e322bb4becd7bcff7f86d592edf68
erdemirbehiye <behiyeerdemir@gmail.com>
2024-01-26 22:31:30 +0100

    add sticky header and change the background

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="sticky-header bg-white border-b border-black py-5 z-10">
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div className="text-2xl ml-16">pathfinder</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
